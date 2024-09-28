67908627ba929c5272275d2138323365a2367a3c
Divyasingh03 <divya954.singh@gmail.com>
2024-09-28 13:22:45 +0530

    Update and rename vaultCurrency.sol to vault.sol

--- vault.sol
+++ vault.sol
@@ -3,11 +3,20 @@ pragma solidity ^0.8.17;
 
 interface IERC20 {
     function totalSupply() external view returns (uint);
+
     function balanceOf(address account) external view returns (uint);
+
     function transfer(address recipient, uint amount) external returns (bool);
+
     function allowance(address owner, address spender) external view returns (uint);
+
     function approve(address spender, uint amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
 
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
@@ -18,9 +27,6 @@ contract Vault {
 
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
-
-    event Deposit(address indexed user, uint amount, uint shares);
-    event Withdraw(address indexed user, uint shares, uint amount);
 
     constructor(address _token) {
         token = IERC20(_token);
@@ -37,28 +43,40 @@ contract Vault {
     }
 
     function deposit(uint _amount) external {
-        require(_amount > 0, "Vault: deposit amount must be greater than zero");
+        /*
+        a = amount
+        B = balance of token before deposit
+        T = total supply
+        s = shares to mint
+
+        (T + s) / T = (a + B) / B 
+
+        s = aT / B
+        */
         uint shares;
-        uint balance = token.balanceOf(address(this));
         if (totalSupply == 0) {
             shares = _amount;
         } else {
-            shares = (_amount * totalSupply) / balance;
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
         }
-        require(shares > 0, "Vault: shares must be greater than zero");
 
         _mint(msg.sender, shares);
         token.transferFrom(msg.sender, address(this), _amount);
-        emit Deposit(msg.sender, _amount, shares);
     }
 
     function withdraw(uint _shares) external {
-        require(_shares > 0, "Vault: withdraw shares must be greater than zero");
-        require(balanceOf[msg.sender] >= _shares, "Vault: insufficient shares");
+        /*
+        a = amount
+        B = balance of token before withdraw
+        T = total supply
+        s = shares to burn
 
+        (T - s) / T = (B - a) / B 
+
+        a = sB / T
+        */
         uint amount = (_shares * token.balanceOf(address(this))) / totalSupply;
         _burn(msg.sender, _shares);
         token.transfer(msg.sender, amount);
-        emit Withdraw(msg.sender, _shares, amount);
     }
 }
